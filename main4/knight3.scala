2e3a4b4cb518a79e362ef4cc548d2bbfc887865f
dariana <darianadorin@yahoo.com>
2022-01-18 20:33:30 +0000

    k3

--- main4/knight3.scala
+++ main4/knight3.scala
@@ -10,6 +10,7 @@ object M4c {
 // implement it, but do not make any changes to the
 // templates below.
 
+import scala.annotation.tailrec
 
 type Pos = (Int, Int)    // a position on a chessboard 
 type Path = List[Pos]    // a path...a list of positions
@@ -20,7 +21,37 @@ type Path = List[Pos]    // a path...a list of positions
 //    and starting field (0, 0). It has to produce a solution within
 //    30 seconds.
 
+def is_legal(dim: Int, path: Path, x: Pos) : Boolean = {
+  !path.contains(x) &&
+          x._1>=0 && x._1<dim && 
+          x._2>=0 && x._2<dim
+}
 
-def tour_on_mega_board(dim: Int, path: Path) : Option[Path] = ???
+ 
+def legal_moves(dim: Int, path: Path, x: Pos) : List[Pos] = {
+  List( 
+    (x._1 +1 , x._2 +2),
+    (x._1 +2 , x._2 +1),
+    (x._1 +2 , x._2 -1),
+    (x._1 +1 , x._2 -2),
+    (x._1 -1 , x._2 -2),
+    (x._1 -2 , x._2 -1),
+    (x._1 -2 , x._2 +1),
+    (x._1 -1 , x._2 +2),
+  ).filter(x => is_legal(dim,path,x))
+}
+
+
+def ordered_moves(dim: Int, path: Path, x: Pos) : List[Pos] = {
+    legal_moves(dim,path,x).sortBy(legal_moves(dim,path,_).length)
+}
+
+@tailrec
+def tour_on_mega_board(dim: Int, path: Path) : Option[Path] = {
+    if(path.length == dim*dim) Some(path)
+    else 
+        tour_on_mega_board(dim,ordered_moves(dim,path,path.head).head::path)
+}
 
 }
+//tour_on_mega_board(70, List((0, 0)))
